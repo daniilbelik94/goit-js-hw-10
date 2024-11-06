a055df56ec3b32d3c983e2ea41ec7df6d9c33d0c
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2024-11-06 13:15:43 +0100

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -6,8 +6,6 @@ import errorMessage from '../img/error-massage.svg';
 
 const datetimePicker = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
-// const errorMessage = document.querySelector('.error-message');
-
 const dataDays = document.querySelector('span[data-days]');
 const dataHours = document.querySelector('span[data-hours]');
 const dataiMnutes = document.querySelector('span[data-minutes]');
@@ -15,6 +13,9 @@ const dataSeconds = document.querySelector('span[data-seconds]');
 
 let curentData = new Date();
 let userSelectedDate = null;
+
+// btn blocked after click
+startBtn.disabled = true;
 
 startBtn.addEventListener('click', onStartTimerClick);
 
@@ -24,10 +25,10 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < curentData) {
+    if (selectedDates[0] < new Date()) {
       iziToast.show({
         title: 'Error',
-        message: 'Please choose a data in the future',
+        message: 'Please choose a date in the future',
         position: 'topRight',
         backgroundColor: '#ef4040',
         titleColor: '#fff',
@@ -39,42 +40,30 @@ const options = {
         iconUrl: errorMessage,
         timeout: 5000,
       });
-      // errorMassage.classList.add('active');
       startBtn.disabled = true;
     } else {
-      userSelectedDate = new Date(selectedDates[0]);
+      userSelectedDate = selectedDates[0];
       startBtn.disabled = false;
-      // errorMassage.classList.remove('active');
     }
   },
 };
 
+//  flatpickr
 flatpickr(datetimePicker, options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
   const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
@@ -88,14 +77,21 @@ function updateTimer({ days, hours, minutes, seconds }) {
 }
 
 function onStartTimerClick() {
+  // Заблокируем кнопку и инпут при запуске таймера
   startBtn.disabled = true;
+  datetimePicker.disabled = true;
+
   const intervalId = setInterval(() => {
     curentData = new Date();
     const differentTime = userSelectedDate - curentData;
     const time = convertMs(differentTime);
     updateTimer(time);
-    if (Math.floor(differentTime / 1000) === 0) {
+
+    if (differentTime <= 0) {
       clearInterval(intervalId);
+
+      // unblock button and input
+      datetimePicker.disabled = false;
     }
   }, 1000);
 }
